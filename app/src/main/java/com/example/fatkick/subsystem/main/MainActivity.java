c8dbd5363e67a97578714689a4a448f6f3f81e88
tuba <tasnuvatuba99@gmail.com>
2022-04-18 19:18:25 +0600

    added some log messages

--- app/src/main/java/com/example/fatkick/subsystem/main/MainActivity.java
+++ app/src/main/java/com/example/fatkick/subsystem/main/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("output", "main activity opened");
 
 
         bindUI();
